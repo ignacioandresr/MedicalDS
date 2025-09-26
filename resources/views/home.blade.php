ac9496364bd121b27016a3e224fbb13bb6e64820
ignacioandresr <ignacioandresreynado@gmail.com>
2025-09-26 15:23:00 -0300

    a

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,9 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">
+                    <p class="fw-bold fs-6">Bienvenido</p>
+                </div>
 
                 <div class="card-body">
                     @if (session('status'))
